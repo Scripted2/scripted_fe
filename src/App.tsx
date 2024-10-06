eb7644e1dd24b0a2fa7d981fccafd27704c6d150
mane <tomevmarjan33@gmail.com>
2024-10-06 13:48:21 +0200

    feature:  - added global background  - created the login and signup pages  - implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import {ChakraProvider} from "@chakra-ui/react";
 
+import "./styles/global.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {IRoute} from "./interfaces/route.interface.ts";
+import {useRoutesService} from "./services/routes.services.ts";
+
 function App() {
+    const routes: IRoute[] = useRoutesService();
 
     return (
         <>
             <ChakraProvider>
+                <BrowserRouter>
+                    <Routes>
+                        {routes.map((route) => (
+                            <Route
+                                key={route.name}
+                                path={route.path}
+                                element={<route.element/>}
+                            />
+                        ))}
+                    </Routes>
+                </BrowserRouter>
             </ChakraProvider>
         </>
     )
---
+++ src/services/routes.services.ts
@@ -0,0 +1,23 @@
+import {IRoute} from "../interfaces/route.interface.ts";
+import LogInPage from "../pages/auth/LogIn.tsx";
+import SignUpPage from "../pages/auth/SignUp.tsx";
+
+
+export const useRoutesService = (): IRoute[] => {
+    const unprotectedRoutes: IRoute[] = [
+        {
+            path: "/login",
+            name: "Login",
+            element: LogInPage
+        },
+        {
+            path: "/signup",
+            name: "Sign Up",
+            element: SignUpPage
+        }
+    ];
+
+    const protectedRoutes: IRoute[] = [];
+
+    return [...unprotectedRoutes, ...protectedRoutes];
+}
